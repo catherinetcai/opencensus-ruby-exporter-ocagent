3bbba21a7b2fa29d6d68dff410031df165fb29e3
Cat Cai <catherinetcai@gmail.com>
2021-09-23 21:46:30 +0100

    Adding some test helpers

--- lib/opencensus/trace/exporters/ocagent.rb
+++ lib/opencensus/trace/exporters/ocagent.rb
@@ -36,7 +36,15 @@ module OpenCensus
       class OCAgent
         attr_reader :client, :endpoint, :node, :service_name
 
-        def initialize(service_name:, host_name: nil, endpoint: nil, client: nil)
+        ## Create an OpenCensusTrace exporter
+        #
+        # @param [String] :service_name The name of the service to report in traces.
+        # @param [optional String] :host_name The host_name of the node.
+        # @param [optional String] :endpoint The endpoint to forward the OpenCensus traces.
+        # @param [optional Gruf::Client] :client The gRPC client used to export OC traces.
+        # @param [optional Integer] :max_queue The max number of API requests that can be queued.
+        # @param [optional Integer] :max_threads the max number of threads to process requests.
+        def initialize(service_name:, host_name: nil, endpoint: nil, client: nil, max_queue: 1000, max_threads: 1)
           @endpoint = endpoint || DEFAULT_ENDPOINT
           @client = client || ::Gruf::Client.new(
             service: ::OpenCensus::Proto::Agent::Trace::V1::TraceService,
@@ -46,19 +54,19 @@ module OpenCensus
           @node = get_node(service_name: @service_name, host_name: host_name)
         end
 
-        def emit(span_datas)
-          responses = client.call(:Export, generate_span_requests(span_datas)) # This is where the span requests go
+        ## Export spans to OpenCensus synchronously, for now.
+        #
+        # @param [Array<OpenCensus::Trace::Span>] :spans The captured spans to forward to the trace server.
+        #
+        def export(spans)
+          responses = client.call(:Export, generate_span_requests(spans)) # This is where the span requests go
           puts("Responses: #{responses}")
         rescue ::Gruf::Client::Error => e
           puts("Error: #{e}")
         end
 
-        def export(span_datas)
-          emit(span_datas)
-        end
-
-        def generate_span_requests(span_datas)
-          span_protos = span_datas.map { |span_data| Converter.new(span_data) }
+        def generate_span_requests(spans)
+          span_protos = span_datas.map { |span_data| Converter.new(spans) }
 
           ::OpenCensus::Proto::Agent::Trace::V1::ExportTraceServiceRequest.new(node: node, spans: span_protos)
         end
